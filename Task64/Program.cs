36b1b680cf4d5e57ac6719b868ec63abe394a005
Choppa <denis.afinogenow@yandex.ru>
2022-07-21 22:06:47 +0300

    first commit

---
+++ Task64/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+
+int Numbers(int n)
+{
+    if (n/10 != 0) return Numbers(n / 10) + 1;
+    else return 1;
+}
+Console.WriteLine("Input number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Numbers(n));
---
+++ Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.;
+
+int SumOfNum(int m, int n)
+{
+    if (m < n)
+    {
+        return n + SumOfNum(m,n-1);
+    }
+    else return n;
+}
+Console.WriteLine("Input first number");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumOfNum(m,n));
